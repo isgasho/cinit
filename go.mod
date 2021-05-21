a7d063013c72a7d9129c48bd30e8a788e9b96ef0
James Mills <prologic@shortcircuit.net.au>
2021-05-22 00:05:26 +1000

    Add support for pty(s)

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/prologic/cinit
 go 1.16
 
 require (
+	github.com/creack/pty v1.1.12 // indirect
 	github.com/ijustfool/docker-secrets v0.0.0-20191021062307-b25ea5007562 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
+	golang.org/x/term v0.0.0-20210503060354-a79de5458b56 // indirect
 )
--- main.go
+++ main.go
@@ -10,8 +10,11 @@ import (
 	"strings"
 	"syscall"
 
+	"github.com/creack/pty"
 	secrets "github.com/ijustfool/docker-secrets"
+	isatty "github.com/mattn/go-isatty"
 	log "github.com/sirupsen/logrus"
+	"golang.org/x/term"
 )
 
 func envKey(s string) string {
@@ -36,7 +39,6 @@ func main() {
 	}
 
 	cmd := exec.Command(os.Args[1], os.Args[2:]...)
-	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
 
 	dockerSecrets, err := secrets.NewDockerSecrets("")
 	if err != nil {
@@ -45,38 +47,87 @@ func main() {
 		cmd.Env = mapToEnvList(dockerSecrets.GetAll())
 	}
 
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		log.WithError(err).Fatal("error creating stdout pipe")
-	}
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		log.WithError(err).Fatal("error creating stderr pipe")
-	}
-
-	err = cmd.Start()
-	if err != nil {
-		log.WithError(err).Fatal("error starting process")
-	}
-
-	child, err := syscall.Getpgid(cmd.Process.Pid)
-	if err != nil {
-		log.WithError(err).Fatal("error getting process group")
-	}
-
-	go io.Copy(os.Stdout, stdout)
-	go io.Copy(os.Stderr, stderr)
-
-	sig := make(chan os.Signal)
-	signal.Notify(sig)
-	signal.Ignore(syscall.SIGCHLD)
-
-	// Handle zombies
-	go func() {
-		for {
-			syscall.Kill(-(child), (<-sig).(syscall.Signal))
+	if isatty.IsTerminal(os.Stdout.Fd()) {
+		ptmx, err := pty.Start(cmd)
+		if err != nil {
+			log.WithError(err).Warn("error allocating pty")
 		}
-	}()
+		// Make sure to close the pty at the end.
+		defer func() { _ = ptmx.Close() }() // Best effort.
+
+		// Handle pty size.
+		ch := make(chan os.Signal, 1)
+		signal.Notify(ch, syscall.SIGWINCH)
+		go func() {
+			for range ch {
+				if err := pty.InheritSize(os.Stdin, ptmx); err != nil {
+					log.Printf("error resizing pty: %s", err)
+				}
+			}
+		}()
+		ch <- syscall.SIGWINCH                        // Initial resize.
+		defer func() { signal.Stop(ch); close(ch) }() // Cleanup signals when done.
+
+		// Set stdin in raw mode.
+		oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+		if err != nil {
+			panic(err)
+		}
+		defer func() { _ = term.Restore(int(os.Stdin.Fd()), oldState) }() // Best effort.
+
+		// Copy stdin to the pty and the pty to stdout.
+		// NOTE: The goroutine will keep reading until the next keystroke before returning.
+		go func() { _, _ = io.Copy(ptmx, os.Stdin) }()
+		_, _ = io.Copy(os.Stdout, ptmx)
+	} else {
+		cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+
+		stdin, err := cmd.StdinPipe()
+		if err != nil {
+			log.WithError(err).Fatal("error creating stdin pipe")
+		}
+
+		stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			log.WithError(err).Fatal("error creating stdout pipe")
+		}
+		stderr, err := cmd.StderrPipe()
+		if err != nil {
+			log.WithError(err).Fatal("error creating stderr pipe")
+		}
+
+		err = cmd.Start()
+		if err != nil {
+			log.WithError(err).Fatal("error starting process")
+		}
+
+		child, err := syscall.Getpgid(cmd.Process.Pid)
+		if err != nil {
+			log.WithError(err).Fatal("error getting process group")
+		}
+
+		go func() {
+			_, err := io.Copy(stdin, os.Stdin)
+			if err != nil && err != io.EOF {
+				log.WithError(err).Error("error reading stdin pipe")
+			}
+			cmd.Process.Kill()
+		}()
+
+		go io.Copy(os.Stdout, stdout)
+		go io.Copy(os.Stderr, stderr)
+
+		sig := make(chan os.Signal)
+		signal.Notify(sig)
+		signal.Ignore(syscall.SIGCHLD)
+
+		// Handle zombies
+		go func() {
+			for {
+				syscall.Kill(-(child), (<-sig).(syscall.Signal))
+			}
+		}()
+	}
 
 	var status syscall.WaitStatus
 	syscall.Wait4(-1, &status, 0, nil)
